fb4e210be5807e397cad71dfc9942ad5a99af003
Gurgen <gsarkisdganyan@gmail.com>
2022-01-17 22:48:44 +0300

    Route, BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
+import Dialogs from './components/Dialogs/Dialogs';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import {Routes} from "react-router-dom";
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
+
 
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-            <Profile/>
-        </div>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className="app-wrapper-content">
+                    <Routes>
+                        <Route element={<Profile/>} path='/profile'/>
+                        <Route element={<Dialogs/>} path='/dialogs'/>
+                        <Route element={<Music/>} path='/music'/>
+                        <Route element={<News/>} path='/news' />
+                        <Route element={<Settings/>} path='/settings' />
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
     )
 }
 
---
+++ src/components/Music/Music.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+const Music = () => {
+    return (
+        <div>music</div>
+    )
+
+}
+export default Music;
